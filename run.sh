96def4e9c207694af142e0f154e896adb947a4b4
Bochen Dong <35963069+bochendong@users.noreply.github.com>
2024-07-02 12:32:28 -0400

    update

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -A bif146
+#SBATCH -o gpu_check_output.o%J
+#SBATCH -t 02:00:00
+#SBATCH -N 1
+#SBATCH -p batch
+#
+
+export MIOPEN_DISABLE_CACHE=1
+export MIOPEN_CUSTOM_CACHE_DIR='pwd'
+export HOME="/tmp/srun"
+
+module load PrgEnv-gnu
+module load gcc/12.2.0
+module load rocm/5.7.0
