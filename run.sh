0581bd21fd68be2102596f2bb9ff56b18286f2ea
Bochen Dong <35963069+bochendong@users.noreply.github.com>
2024-07-02 12:48:55 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,6 +12,6 @@ export HOME="/tmp/srun"
 
 module load PrgEnv-cray/8.3.3
 module load cce/15.0.0
-module load rocm/5.7
+module load rocm/5.7.0
 
 python /lustre/orion/bif146/world-shared/enzhi/baby_llama/DistributedDataParallel/Torch_version.py
