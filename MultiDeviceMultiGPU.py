0436d32eb0b682ffdfb7c9125aff1895e221129e
Bochen Dong <35963069+bochendong@users.noreply.github.com>
2024-07-12 14:12:59 -0400

    Update MultiDeviceMultiGPU.py

--- MultiDeviceMultiGPU.py
+++ MultiDeviceMultiGPU.py
@@ -1,34 +1,21 @@
+import os
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, DistributedSampler
+from torch import nn
+import torch.utils.data as data
 from torchvision import datasets, transforms
-from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.multiprocessing as mp
 import torch.distributed as dist
-import os
+from torch.nn.parallel import DistributedDataParallel as DDP
+import logging
 import argparse
 
-def check_available_gpus():
-    try:
-        num_gpus = torch.cuda.device_count()
-        if num_gpus == 0:
-            print("No GPUs available.")
-        else:
-            print(f"Number of available GPUs: {num_gpus}")
-            for i in range(num_gpus):
-                print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
-
-        return num_gpus
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        print("Make sure PyTorch is installed with ROCm support.")
-
-def init_process(rank, world_size, master_addr, master_port, train_fn, backend='nccl'):
-    os.environ['MASTER_ADDR'] = master_addr
-    os.environ['MASTER_PORT'] = master_port
-    dist.init_process_group(backend, rank=rank, world_size=world_size)
-    train_fn(rank, world_size)
+# Configure logging
+def log(args):
+    logging.basicConfig(
+        filename=args.logname,
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
 
 class SimpleNN(nn.Module):
     def __init__(self):
@@ -44,51 +31,119 @@ class SimpleNN(nn.Module):
         x = self.fc3(x)
         return x
 
-def train(rank, world_size):
-    # Set device and seed
-    torch.manual_seed(0)
-    device = torch.device(f'cuda:{rank % torch.cuda.device_count()}')
-    
-    # Load data
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-    dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
-    sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank)
-    dataloader = DataLoader(dataset, batch_size=64, sampler=sampler)
-    
-    # Build model and move it to GPU
-    model = SimpleNN().to(device)
-    ddp_model = DDP(model, device_ids=[rank % torch.cuda.device_count()])
-    
-    # Define loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001, momentum=0.9)
-    
-    # Train the model
-    ddp_model.train()
-    for epoch in range(10):
-        sampler.set_epoch(epoch)
-        for batch_idx, (data, target) in enumerate(dataloader):
-            data, target = data.to(device), target.to(device)
-            
+def train_model(model, train_loader, val_loader, criterion, optimizer, num_epochs, device_id):
+    model.train()
+    total_step = len(train_loader)
+    best_val_acc = 0.0
+    logging.info("Training the SimpleNN model for %d epochs...", num_epochs)
+
+    for epoch in range(num_epochs):
+        logging.info("Epoch %d/%d", epoch + 1, num_epochs)
+        running_loss = 0.0
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.to(device_id)
+            labels = labels.to(device_id)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
             optimizer.zero_grad()
-            output = ddp_model(data)
-            loss = criterion(output, target)
             loss.backward()
             optimizer.step()
-            
-            if batch_idx % 100 == 0:
-                print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(dataloader.dataset)}] Loss: {loss.item()}')
+
+            running_loss += loss.item()
+            if i % 100 == 99 and device_id == 0:
+                logging.info('[%d, %5d] loss: %.3f', epoch + 1, i + 1, running_loss / 100)
+                running_loss = 0.0
+
+        val_acc = evaluate_model(model, val_loader, device_id)
+        logging.info("Epoch: %d, Validation Accuracy: %.4f", epoch + 1, val_acc)
+
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            torch.save(model.state_dict(), "best_simple_nn_model.pth")
+
+        logging.info('Finished Training Step %d', epoch + 1)
+
+    logging.info('Finished Training. Best Validation Accuracy: %.4f', best_val_acc)
+
+def evaluate_model(model, val_loader, device_id):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images = images.to(device_id)
+            labels = labels.to(device_id)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    return accuracy
+
+def simple_nn_train(args):
+    local_rank = int(os.environ['SLURM_LOCALID'])
+    os.environ['MASTER_ADDR'] = str(os.environ['HOSTNAME'])
+    os.environ['MASTER_PORT'] = "29500"
+    os.environ['WORLD_SIZE'] = os.environ['SLURM_NTASKS']
+    os.environ['RANK'] = os.environ['SLURM_PROCID']
+    print("MASTER_ADDR:{}, MASTER_PORT:{}, WORLD_SIZE:{}, WORLD_RANK:{}, local_rank:{}".format(os.environ['MASTER_ADDR'], 
+                                                    os.environ['MASTER_PORT'], 
+                                                    os.environ['WORLD_SIZE'], 
+                                                    os.environ['RANK'],
+                                                    local_rank))
+    dist.init_process_group(
+        backend='nccl',
+        init_method='env://',
+        world_size=args.world_size,
+        rank=int(os.environ['RANK'])
+    )
+    print("SLURM_LOCALID/local_rank:{}, dist_rank:{}".format(local_rank, dist.get_rank()))
+
+    print(f"Start running basic DDP example on rank {local_rank}.")
+    device_id = local_rank % torch.cuda.device_count()
+    
+    # Create DataLoader for training and validation
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    val_dataset = datasets.MNIST('./data', train=False, download=True, transform=transform)
+
+    train_sampler = data.DistributedSampler(train_dataset, num_replicas=args.world_size, rank=local_rank)
+    val_sampler = data.DistributedSampler(val_dataset, num_replicas=args.world_size, rank=local_rank)
+
+    train_loader = data.DataLoader(dataset=train_dataset, batch_size=64, sampler=train_sampler)
+    val_loader = data.DataLoader(dataset=val_dataset, batch_size=64, sampler=val_sampler)
+
+    # Create SimpleNN model
+    model = SimpleNN()
+    model.to(device_id)
+    model = DDP(model, device_ids=[device_id])
+
+    # Define loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters())
+
+    # Train the model
+    train_model(model, train_loader, val_loader, criterion, optimizer, args.num_epochs, device_id=device_id)
+    dist.destroy_process_group()
+
+def simple_nn_ddp(args):
+    log(args=args)
+    args.world_size = int(os.environ['SLURM_NTASKS'])
+    # mp.spawn(simple_nn_train, nprocs=args.gpus, args=(args,))
+    simple_nn_train(args=args)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--nodes', type=int, default=1, help='number of nodes')
     parser.add_argument('--gpus', type=int, default=1, help='number of gpus per node')
     parser.add_argument('--nr', type=int, default=0, help='ranking within the nodes')
-    parser.add_argument('--master_addr', type=str, default='localhost', help='address of the master node')
-    parser.add_argument('--master_port', type=str, default='29500', help='port of the master node')
+    parser.add_argument('--logname', type=str, default='training.log', help='log file name')
+    parser.add_argument('--num_epochs', type=int, default=10, help='number of epochs to train')
+    parser.add_argument('--pretrained', type=bool, default=False, help='use pretrained model or not')
     args = parser.parse_args()
-    
-    world_size = args.gpus * args.nodes
-    rank = args.nr * args.gpus
-    for gpu in range(args.gpus):
-        mp.spawn(init_process, nprocs=args.gpus, args=(world_size, args.master_addr, args.master_port, train))
+
+    args.world_size = args.gpus * args.nodes
+    mp.spawn(simple_nn_ddp, nprocs=args.gpus, args=(args,))
